be4b4c08c311fcc1e083817f873338cae143716a
Maxio <29357824+ItsMaxio@users.noreply.github.com>
2022-11-21 21:33:20 +0100

    docker support

--- src/main/java/net/ciastkarnia/App.java
+++ src/main/java/net/ciastkarnia/App.java
@@ -10,8 +10,35 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class App {
     public static void main(String[] args) throws Exception {
-            Dotenv dotenv = Dotenv.load();
-            String token = dotenv.get("TOKEN");
+
+            String token = null;
+            if (args.length == 0) {
+                System.out.println("Trying to use .env file!");
+
+                Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+                token = dotenv.get("TOKEN");
+
+                if (token == null || token.length() == 0) {
+                    System.out.println(".env file doesnt exist or is corrupted. Use arguments after .jar!");
+                    return;
+                }
+                System.out.println("Token = " + token);
+            }
+            else{
+                System.out.println("Trying to use arguments after .jar!");
+                
+                for (String arg : args) {
+                    if (!(arg.contains("--token"))) continue;
+
+                    token = arg.substring(arg.indexOf("=")+1);
+                }
+                
+                if (token == null || token.length() == 0) {
+                    System.out.println("Invalid TOKEN, use --token=\"\"");
+                    return;
+                }
+                System.out.println("Token = " + token);
+            }
 
             JDA bot = JDABuilder.createDefault(token, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES)
             .addEventListeners(new Listener())
